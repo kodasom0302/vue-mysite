7a089a94928db8d6047300ae4fffd8b068f67c93
kodasom0302 <dasom589@gmail.com>
2024-04-23 15:24:23 +0900

    2024-04-23

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div id="footer">
+        Copyright ⓒ 2024 고다솜. All right reserved
+        </div>
+       <!-- //footer -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AppFooter",
+    components: {},
+    data() {
+        return {};
+    },
+    methods: {},
+    created() { }
+};
+</script>
